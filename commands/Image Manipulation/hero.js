934f62247d8b79d8d3537ddb0834ba37e69ca19a
Tanner Reynolds <TannerReynolds@Outlook.com>
2019-01-31 20:29:40 -0800

    added hero command

--- commands/Image Manipulation/hero.js
+++ commands/Image Manipulation/hero.js
@@ -1,15 +1,15 @@
 const argHandler = require("../../util/imgHandler.js")
 module.exports = {
-    command:"f",
-    description: "Press f to pay respects",
-    syntax: ")>f `|` )>f [Attachment | User Name | User ID | User Mention | Image URL | Emoji ]",
+    command:"hero",
+    description: "zen -> zero -> hen -> hero",
+    syntax: ")>hero `|` )>hero [Attachment | User Name | User ID | User Mention | Image URL | Emoji ]",
     category: "Image Manipulation",
     permission: "sendMessages",
     botPermission: "attachFiles",
     execute:async (bot, msg, args, commands, logger, c, s) => {
         argHandler.handle(bot, msg, args, renderImage);
         async function renderImage(img) {
-            s.get(`http://${c.havana}/canvas?url=${img}&effect=f`).then(r => msg.channel.createMessage("", {file: r.body, name: "render.jpg"}))
+            s.get(`http://${c.havana}/canvas?url=${img}&effect=hero`).then(r => msg.channel.createMessage("", {file: r.body, name: "render.jpg"}))
         }
     }
 }
